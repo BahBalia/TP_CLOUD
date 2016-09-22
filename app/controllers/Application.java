394b30b6ba449a8c284a61deb6c04f768e1f5139
BahBalia <bahbalia90@yahoo.fr>
2016-09-22 16:08:25 +0200

    Gestios Taches

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,61 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
+import play.mvc.Controller;
 import java.util.*;
-
-import models.*;
+import models.Tache;
+import play.data.*;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 public class Application extends Controller {
 
-    public static void index(String nom) {
-	String prenom ="Balia";
-        render(nom,prenom);
+	// Affiche toutes les tâches (voir variable taches) dans le template views/listTache.html
+    public static void listTache() {
+        List<Tache> taches = Tache.findAll();
+	render(taches);
     }
-	public static void bonjour(String nom, String prenom){
-		render(nom,prenom);
-	}
+
+    // Affiche le template views/ajouterTacheForm.html (formulaire d'ajout d'une tâche)
+    public static void ajouterTacheForm() {
+        render();
+    }
+
+    // Ajoute une nouvelle tâche en base de données et affiche le template views/ajouterTache.html
+    public static void ajouterTache(String libelle, Boolean statut, String description) {
+        Tache tache = new Tache();
+	tache.libelle = libelle;
+	if(statut != null)
+		tache.statut = true;
+	else
+		tache.statut = false;	
+	tache.description = description;
+	tache.save();
+	render();
+	
+    }
+
+    // Change le statut d'une tâche en base de données
+    public static void validerTache(Long id) {
+    	Tache t = Tache.findById(id);
+	t.statut = true;
+	t.save();
+	render();
+    }
+
+    // Supprime une tâche en base de données
+    public static void supprimerTache(Long id) {
+       Tache t = Tache.findById(id);
+	System.out.println("id:" + id);
+	t.delete();
+    }
+
+    // Modifie une tâche en base de données
+    public static void editTache(Long id, String title) {
+        Tache t = Tache.findById(id);
+	t.libelle = title;
+	t.save();
+	renderJSON(t);
+    }
 }
